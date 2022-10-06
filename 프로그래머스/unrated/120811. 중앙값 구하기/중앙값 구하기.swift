fb19a924b2c8340b0f2014e0f430fb7cb7664e18
mxnkng <107797217+mxnkng@users.noreply.github.com>
2022-10-06 19:22:43 +0900

    [unrated] Title: 중앙값 구하기, Time: 0.08 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.swift
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.swift
@@ -1,12 +1,8 @@
 import Foundation
-
+// 조건에서 array의 길이가 홀수이므로 짝수인 경우를 고려하지 않아도 된다!
+// 코드 수정
 func solution(_ array:[Int]) -> Int {
     let sortedArray = array.sorted()
-    let answer : Int
-    if sortedArray.count % 2 == 0 {
-        answer = sortedArray[sortedArray.count / 2 - 1]
-    } else {
-        answer = sortedArray[sortedArray.count / 2]
-    }
+    let answer = sortedArray[sortedArray.count / 2]
     return answer
 }
