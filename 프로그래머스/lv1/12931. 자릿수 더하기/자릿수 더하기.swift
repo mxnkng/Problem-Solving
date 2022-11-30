09522b650220bef7d213aeb12fddb24fc0af67ff
이민경 <107797217+mxnkng@users.noreply.github.com>
2022-11-30 22:32:18 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int) -> Int{
+    var number: Int = n
+    var sum: Int = 0
+    while number > 0 {
+        sum += number % 10
+        number /= 10
+    }
+    return sum
+}
